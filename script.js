7fd99229ae6fe9284f4eae9f39c4bb318138d5ab
vanessatelter08 <vanessa.stelter.silva@escola.pr.gov.br>
2025-06-12 12:12:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function soma(){
+
+  let numero_um = parseInt (document.getElementById('primeiro_numero').value);
+  let numero_dois = parseInt (document.getElementById('segundo_numero').value);
+
+ let soma_numero = numero_um + numero_dois;
+ let subtrai_numero = numero_um + numero_dois;
+ 
+document.getElementById('resultado').textContent = 'resultado' + soma_numero;
+ 
+function subtrair(){
+
+
+}
+
+}
